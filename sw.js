aa186908d85d642a4f5bcbbbc933e12288be9c40
andreavignoni82-spec <andrea.vignoni82@gmail.com>
2026-08-12 09:19:20 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-const CACHE='spurgoflow-suite-v5-5.0.7';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./qr-demo-intervento.png']))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE='spurgoflow-suite-v5-5.0.8';
+const LOCAL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./qr-demo-intervento.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(LOCAL))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
+self.addEventListener('fetch',e=>{
+ const u=new URL(e.request.url);
+ if(u.hostname.includes('openstreetmap.org')||u.hostname.includes('unpkg.com')){
+   e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+   return;
+ }
+ e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
