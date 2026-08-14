9ed88f3cbcb6bd24f7d09d9fa129a177cea967b2
andreavignoni82-spec <andrea.vignoni82@gmail.com>
2026-08-14 19:48:01 +0200

    Bump PWA cache for Spurgo Flow 8.1 UI

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'spurgoflow-v8-0.0-beta.1.1-boot-core-fix';
+const CACHE_NAME = 'spurgoflow-v8-1-0-unified-ui';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
-self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))));
+self.addEventListener('install', (event) => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))); });
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key.startsWith('spurgoflow-v8') && key !== CACHE_NAME).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
